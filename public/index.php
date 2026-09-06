7ef55bc4b408651de451561207a6e00461532c23
Fabien Heilles <phabi1@hotmail.fr>
2026-09-06 14:50:18 +0200

    fix environment

--- public/index.php
+++ public/index.php
@@ -2,8 +2,13 @@
 
 require __DIR__ . '/../vendor/autoload.php';
 
+$env = getenv('APP_ENV');
+if (!$env) {
+    $env = 'production';
+}
+
+define('APP_ENV', $env);
 define('APP_DIR', __DIR__ . '/..');
-define('APP_ENV', getenv('APP_ENV') ?? 'production');
 define('CONFIG_DIR', APP_DIR . '/config');
 define('CACHE_DIR', APP_DIR . '/cache');
 
